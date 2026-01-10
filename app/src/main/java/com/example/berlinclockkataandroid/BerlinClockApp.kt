856aec9cc171660c5f54f7cfeaff9ace533de153
Pooja Jardosh <pooja2011er@gmail.com>
2026-01-10 20:08:46 +0530

    Hilt setup & BerlinClockApp class added

---
+++ app/src/main/java/com/example/berlinclockkataandroid/BerlinClockApp.kt
@@ -0,0 +1,9 @@
+package com.example.berlinclockkataandroid
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+class BerlinClockApp {
+    @HiltAndroidApp
+    class BerlinClockApp : Application()
+}
