76254a396793751b62c98cd7e3d015d33c07fb26
Pooja Jardosh <pooja2011er@gmail.com>
2026-01-10 13:55:33 +0530

    test: add isValid returns false for empty string

--- app/src/test/java/com/example/berlinclockkataandroid/BerlinClockInputValidatorTest.kt
+++ app/src/test/java/com/example/berlinclockkataandroid/BerlinClockInputValidatorTest.kt
@@ -1,6 +1,7 @@
 package com.example.berlinclockkataandroid
 
 import com.example.berlinclockkataandroid.domain.BerlinClockInputValidator
+import junit.framework.TestCase.assertFalse
 import junit.framework.TestCase.assertTrue
 import org.junit.Test
 
@@ -10,5 +11,10 @@ class BerlinClockInputValidatorTest {
     @Test
     fun `isValid returns true for non-empty string`() {
         assertTrue(validator.isValid("12:56:01"))
+    }
+
+    @Test
+    fun `isValid returns false for empty string`() {
+        assertFalse(validator.isValid(""))
     }
 }
