bb20a78f932e00fab5261d6690cbad738bc16927
Pooja Jardosh <pooja2011er@gmail.com>
2026-01-10 13:31:37 +0530

    Initial commit

---
+++ app/src/main/java/com/example/berlinclockkataandroid/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.berlinclockkataandroid
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.berlinclockkataandroid.ui.theme.BerlinClockKataAndroidTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            BerlinClockKataAndroidTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) {
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    BerlinClockKataAndroidTheme {
+    }
+}
