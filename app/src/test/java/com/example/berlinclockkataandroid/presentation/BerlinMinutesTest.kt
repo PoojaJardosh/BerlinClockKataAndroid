019bb4628a9706a5e2658ce290d43dda51ffec69
Pooja Jardosh <pooja2011er@gmail.com>
2026-01-14 11:37:27 +0530

    test: BerlinMinutesTest getFiveMinuteRow handles Standard Yellows (5, 10 mins)

--- app/src/test/java/com/example/berlinclockkataandroid/presentation/BerlinMinutesTest.kt
+++ app/src/test/java/com/example/berlinclockkataandroid/presentation/BerlinMinutesTest.kt
@@ -15,6 +15,18 @@ class BerlinMinutesTest {
         assertFiveMinuteRow(berlinMinutes.getFiveMinuteRow(4), 0)
     }
 
+    @Test
+    fun `getFiveMinuteRow handles Standard Yellows (5, 10 mins)`() {
+        val lamps5 = berlinMinutes.getFiveMinuteRow(5)
+        assertEquals(BerlinColor.YELLOW, lamps5[0].color)
+        assertEquals(BerlinColor.OFF, lamps5[1].color)
+
+        val lamps10 = berlinMinutes.getFiveMinuteRow(10)
+        assertEquals(BerlinColor.YELLOW, lamps10[0].color)
+        assertEquals(BerlinColor.YELLOW, lamps10[1].color)
+        assertEquals(BerlinColor.OFF, lamps10[2].color)
+    }
+
     private fun assertFiveMinuteRow(lamps: List<Lamp>, onCount: Int) {
         assertEquals(11, lamps.size)
         assertEquals(onCount, lamps.count { it.color != BerlinColor.OFF })
