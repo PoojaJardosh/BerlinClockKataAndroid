38b490f1365c396aee06b7a300bf538593fc6a8b
Pooja Jardosh <pooja2011er@gmail.com>
2026-01-14 10:36:10 +0530

    DI for BirlinSeconds.kt

--- app/src/main/java/com/example/berlinclockkataandroid/ui/clock/BerlinSeconds.kt
+++ app/src/main/java/com/example/berlinclockkataandroid/ui/clock/BerlinSeconds.kt
@@ -1,6 +1,8 @@
 package com.example.berlinclockkataandroid.ui.clock
 
-class BerlinSeconds {
+import javax.inject.Inject
+
+class BerlinSeconds @Inject constructor() {
     fun convert(seconds: Int): Lamp {
         return if (seconds % 2 == 0) {
             Lamp(BerlinColor.YELLOW)
